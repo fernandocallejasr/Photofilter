bfb54981a79265f1202aad467cabfd3501ebbba2
fernandocallejasr <ferd.callejas@gmail.com>
2024-08-29 06:01:17 -0600

    Initial Commit

---
+++ Photofilter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Photofilter
+//
+//  Created by Fernando Callejas on 29/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Photofilter/PhotofilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotofilterApp.swift
+//  Photofilter
+//
+//  Created by Fernando Callejas on 29/08/24.
+//
+
+import SwiftUI
+
+@main
+struct PhotofilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
