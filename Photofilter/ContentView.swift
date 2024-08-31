6f4e22e838275ca1da75a63636e27a41e9744b21
fernandocallejasr <ferd.callejas@gmail.com>
2024-08-31 12:57:01 -0600

    Add two new filters
    Add twon new filters, also hides filter parameter sliders and change filter button until an image is selected

--- Photofilter/ContentView.swift
+++ Photofilter/ContentView.swift
@@ -22,6 +22,8 @@ struct ContentView: View {
     
     @State private var inputUIImage: UIImage?
     @State private var processedImage: Image?
+    @State private var filterScale = 0.5
+    @State private var filterRadius = 0.0
     @State private var filterIntensity = 0.5
     @State private var photoPickerItem: PhotosPickerItem?
     @State private var currentFilter: CIFilter = CIFilter.sepiaTone()
@@ -47,23 +49,34 @@ struct ContentView: View {
                 
                 Spacer()
                 
-                HStack {
-                    Text("Intensity")
-                    Slider(value: $filterIntensity)
-                        .onChange(of: filterIntensity, applyProcessing)
-                }
-                .padding(.vertical)
-                
-                HStack(spacing: 40) {
-                    Button("Change Filter", action: changeFilter)
-                        .buttonStyle(.borderedProminent)
+                if let processedImage {
+                    HStack {
+                        Text("Intensity")
+                        Slider(value: $filterIntensity)
+                            .onChange(of: filterIntensity, applyProcessing)
+                    }
+                    .padding(.top)
                     
-                    if let processedImage {
-                        ShareLink(item: processedImage, preview: SharePreview("Photofilter Image", image: processedImage))
+                    HStack {
+                        Text("Radius")
+                        Slider(value: $filterRadius)
+                            .onChange(of: filterRadius, applyProcessing)
                     }
                     
+                    HStack {
+                        Text("Scale")
+                        Slider(value: $filterScale)
+                            .onChange(of: filterScale, applyProcessing)
+                    }
+                    
+                    HStack(spacing: 40) {
+                        Button("Change Filter", action: changeFilter)
+                            .buttonStyle(.borderedProminent)
+                        
+                        ShareLink(item: processedImage, preview: SharePreview("Photofilter Image", image: processedImage))
+                    }
+                    .padding(.bottom)
                 }
-                .padding(.vertical)
                 
                 PhotosPicker(selection: $photoPickerItem) {
                     Label("Select image", systemImage: "camera.macro.circle")
@@ -72,6 +85,8 @@ struct ContentView: View {
             .padding([.horizontal, .bottom])
             .onChange(of: photoPickerItem, loadImage)
             .confirmationDialog("Select a filter", isPresented: $showingFilters) {
+                Button("Pointillize") { setFilter(CIFilter.pointillize()) }
+                Button("Comic Effect") { setFilter(CIFilter.comicEffect()) }
                 Button("Crystallize") { setFilter(CIFilter.crystallize()) }
                 Button("Edges") { setFilter(CIFilter.edges()) }
                 Button("Gaussian Blur") { setFilter(CIFilter.gaussianBlur()) }
@@ -80,6 +95,8 @@ struct ContentView: View {
                 Button("Unsharp Mask") { setFilter(CIFilter.unsharpMask()) }
                 Button("Vignette") { setFilter(CIFilter.vignette()) }
                 Button("Cancel", role: .cancel) { }
+                
+                
             }
             .navigationTitle("Photofilter")
         }
@@ -104,8 +121,8 @@ struct ContentView: View {
         let inputKeys = currentFilter.inputKeys
         
         if inputKeys.contains(kCIInputIntensityKey) { currentFilter.setValue(filterIntensity, forKey: kCIInputIntensityKey) }
-        if inputKeys.contains(kCIInputRadiusKey) { currentFilter.setValue(filterIntensity * 200, forKey: kCIInputRadiusKey) }
-        if inputKeys.contains(kCIInputScaleKey) { currentFilter.setValue(filterIntensity * 10, forKey: kCIInputScaleKey) }
+        if inputKeys.contains(kCIInputRadiusKey) { currentFilter.setValue(filterRadius * 200, forKey: kCIInputRadiusKey) }
+        if inputKeys.contains(kCIInputScaleKey) { currentFilter.setValue(filterScale * 10, forKey: kCIInputScaleKey) }
         
         guard let outputImage = currentFilter.outputImage else { return }
         guard let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else { return }
