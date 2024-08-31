c64e246999b4236136246266616a1e1ae5a080f0
fernandocallejasr <ferd.callejas@gmail.com>
2024-08-30 22:19:51 -0600

    Initial commit Create initial UI there is a ContentUnavailableView that holds a view while there is no content Add a photo picker for user to add an image Intensity slider an select filter for user to select an user and intensity

--- Photofilter/ContentView.swift
+++ Photofilter/ContentView.swift
@@ -4,18 +4,62 @@
 //
 //  Created by Fernando Callejas on 29/08/24.
 //
-
+import PhotosUI
 import SwiftUI
 
 struct ContentView: View {
+    @State private var processedImage: Image?
+    @State private var filterIntensity = 0.5
+    @State private var photoPickerItem: PhotosPickerItem?
+//    @State private var photoPicker: PhotosPickerItem
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+                Spacer()
+                
+                ContentUnavailableView {
+                    Label("No Image", systemImage: "photo.circle")
+                }
+                
+                processedImage?
+                    .resizable()
+                    .scaledToFit()
+                
+                Spacer()
+                
+                HStack {
+                    Text("Intensity")
+                    Slider(value: $filterIntensity)
+                }
+                .padding(.vertical)
+                
+                HStack {
+                    Button("Change Filter") {
+                        // change filter
+                    }
+                    
+                    Spacer()
+                    
+                    // share the picture
+                }
+                .padding(.vertical)
+                
+                PhotosPicker(selection: $photoPickerItem) {
+                    Label("Please select an image", systemImage: "camera.macro.circle")
+                        .font(.title3)
+                }
+                .tint(.primary)
+            }
+            .padding([.horizontal, .bottom])
+            .onChange(of: photoPickerItem) { oldValue, newValue in
+                Task {
+                    let selectedImage = try await photoPickerItem?.loadTransferable(type: Image.self)
+                    processedImage = selectedImage
+                }
+            }
+            .navigationTitle("Photofilter")
         }
-        .padding()
     }
 }
 
