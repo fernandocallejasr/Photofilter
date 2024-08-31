3101d91413959a6fe65bdd54e19d46ec761d9bd0
fernandocallejasr <ferd.callejas@gmail.com>
2024-08-30 23:06:44 -0600

    Add Filter Add functionality to apply a filter to selected image by user and use an slider to change filter intensity

--- Photofilter/ContentView.swift
+++ Photofilter/ContentView.swift
@@ -4,6 +4,8 @@
 //
 //  Created by Fernando Callejas on 29/08/24.
 //
+import CoreImage
+import CoreImage.CIFilterBuiltins
 import PhotosUI
 import SwiftUI
 
@@ -11,33 +13,38 @@ struct ContentView: View {
     @State private var processedImage: Image?
     @State private var filterIntensity = 0.5
     @State private var photoPickerItem: PhotosPickerItem?
-//    @State private var photoPicker: PhotosPickerItem
+    @State private var currentFilter = CIFilter.sepiaTone()
+    
+    let context = CIContext()
     
     var body: some View {
         NavigationStack {
             VStack {
                 Spacer()
                 
-                ContentUnavailableView {
-                    Label("No Image", systemImage: "photo.circle")
+                PhotosPicker(selection: $photoPickerItem) {
+                    if let processedImage {
+                        processedImage
+                            .resizable()
+                            .scaledToFit()
+                    } else {
+                        ContentUnavailableView {
+                            Label("No Image", systemImage: "photo.circle")
+                        }
+                    }
                 }
-                
-                processedImage?
-                    .resizable()
-                    .scaledToFit()
                 
                 Spacer()
                 
                 HStack {
                     Text("Intensity")
                     Slider(value: $filterIntensity)
+                        .onChange(of: filterIntensity, applyProcessing)
                 }
                 .padding(.vertical)
                 
                 HStack {
-                    Button("Change Filter") {
-                        // change filter
-                    }
+                    Button("Change Filter", action: changeFilter)
                     
                     Spacer()
                     
@@ -52,14 +59,38 @@ struct ContentView: View {
                 .tint(.primary)
             }
             .padding([.horizontal, .bottom])
-            .onChange(of: photoPickerItem) { oldValue, newValue in
-                Task {
-                    let selectedImage = try await photoPickerItem?.loadTransferable(type: Image.self)
-                    processedImage = selectedImage
-                }
-            }
+            .onChange(of: photoPickerItem, loadImage)
             .navigationTitle("Photofilter")
         }
+    }
+    
+    func loadImage() {
+        Task {
+            guard let selectedImage = try await photoPickerItem?.loadTransferable(type: Image.self) else { return }
+            processedImage = selectedImage
+            
+            guard let selectedImageData = try await photoPickerItem?.loadTransferable(type: Data.self) else { return }
+            guard let inputImage = UIImage(data: selectedImageData) else { return }
+            
+            let beginImage = CIImage(image: inputImage)
+            currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+            
+            applyProcessing()
+        }
+    }
+    
+    func applyProcessing() {
+        currentFilter.intensity = Float(filterIntensity)
+        
+        guard let outputImage = currentFilter.outputImage else { return }
+        guard let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else { return }
+        
+        let uiImage = UIImage(cgImage: cgImage)
+        processedImage = Image(uiImage: uiImage)
+    }
+    
+    func changeFilter() {
+        
     }
 }
 
